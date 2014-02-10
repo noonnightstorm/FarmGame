976d668963218924b0ea5b9da5f1ef419a928415
= <=>
2014-02-10 15:46:57 +0800

    move the background

--- Classes/GameBGLayer.cpp
+++ Classes/GameBGLayer.cpp
@@ -21,9 +21,41 @@ GameBGLayer::~GameBGLayer() {
 }
 
 bool GameBGLayer::init(){
+	//设置为可点击
+	if (!CCLayer::init() )
+	{
+		return false;
+	}
+	this->setTouchEnabled(true);
+	//添加背景
 	CCSize size = CCDirector::sharedDirector()->getWinSize();
-	CCSprite* pSprite = CCSprite::create("background.png");
-	pSprite->setPosition( ccp(size.width/2, size.height/2) );
-	this->addChild(pSprite, 0);
+	bGSprite = CCSprite::create("background.png");
+	bGSprite->setPosition( ccp(size.width/2, size.height/2) );
+	this->addChild(bGSprite, 0);
 	return true;
+}
+void GameBGLayer::registerWithTouchDispatcher()
+{
+	CCDirector::sharedDirector()->getTouchDispatcher()->addTargetedDelegate(this,0,true);
+}
+
+
+bool GameBGLayer::ccTouchBegan(CCTouch* touch, CCEvent* event)
+{
+	beginPoint = touch->getLocation();
+	return true;
+}
+
+void GameBGLayer::ccTouchMoved(CCTouch* touch, CCEvent* event)
+{
+	//限制长宽
+	endPoint = touch->getLocation();
+	CCPoint MoveTo = ccpSub(endPoint,beginPoint);
+	beginPoint = endPoint;
+	bGSprite->setPosition(ccpAdd(bGSprite->getPosition(),MoveTo));
+	CCLog("x : %f  |  y : %f",MoveTo.x,MoveTo.y);
+}
+
+void GameBGLayer::ccTouchEnded(CCTouch* touch, CCEvent* event)
+{
 }
