d9e15dbd0ed9b5a7975eba3043633f610371dfdd
= <=>
2014-02-10 15:58:36 +0800

    control the background range

--- Classes/GameBGLayer.cpp
+++ Classes/GameBGLayer.cpp
@@ -48,12 +48,25 @@ bool GameBGLayer::ccTouchBegan(CCTouch* touch, CCEvent* event)
 
 void GameBGLayer::ccTouchMoved(CCTouch* touch, CCEvent* event)
 {
-	//限制长宽
+
 	endPoint = touch->getLocation();
 	CCPoint MoveTo = ccpSub(endPoint,beginPoint);
 	beginPoint = endPoint;
-	bGSprite->setPosition(ccpAdd(bGSprite->getPosition(),MoveTo));
-	CCLog("x : %f  |  y : %f",MoveTo.x,MoveTo.y);
+
+	CCPoint now_position =  bGSprite->getPosition();
+	float now_x = now_position.x;
+	float now_y = now_position.y;
+	CCPoint result_postion = ccpAdd(now_position,MoveTo);
+	//越界
+	if(result_postion.x > 250 || result_postion.x < 230){
+		return;
+	}
+	else if(result_postion.y > 285 || result_postion.y < 35){
+		return;
+	}
+
+	bGSprite->setPosition(ccpAdd(now_position,MoveTo));
+	CCLog("x : %f  |  y : %f",result_postion.x,result_postion.y);
 }
 
 void GameBGLayer::ccTouchEnded(CCTouch* touch, CCEvent* event)
