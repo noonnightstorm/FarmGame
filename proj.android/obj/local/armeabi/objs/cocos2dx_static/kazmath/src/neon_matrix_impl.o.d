f9d4ebbdb915f7a77f8f40a98bd2319566e2806d
noonnightstorm <noonnightstorm@gmail.com>
2014-02-12 18:45:47 +0800

    del pro.android

--- proj.android/obj/local/armeabi/objs/cocos2dx_static/kazmath/src/neon_matrix_impl.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/kazmath/src/neon_matrix_impl.o: \
- /home/noonnightstorm/tool/cocos2d-x-2.2.2/samples/Cpp/farmgame/proj.android/../../../../cocos2dx/kazmath/src/neon_matrix_impl.c \
- /home/noonnightstorm/tool/cocos2d-x-2.2.2/samples/Cpp/farmgame/proj.android/../../../../cocos2dx/kazmath/include/kazmath/neon_matrix_impl.h
-
-/home/noonnightstorm/tool/cocos2d-x-2.2.2/samples/Cpp/farmgame/proj.android/../../../../cocos2dx/kazmath/include/kazmath/neon_matrix_impl.h:
--- proj.android/obj/local/armeabi/objs/cocos2dx_static/support/ccUtils.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/support/ccUtils.o: \
- /home/noonnightstorm/tool/cocos2d-x-2.2.2/samples/Cpp/farmgame/proj.android/../../../../cocos2dx/support/ccUtils.cpp \
- /home/noonnightstorm/tool/cocos2d-x-2.2.2/samples/Cpp/farmgame/proj.android/../../../../cocos2dx/support/ccUtils.h
-
-/home/noonnightstorm/tool/cocos2d-x-2.2.2/samples/Cpp/farmgame/proj.android/../../../../cocos2dx/support/ccUtils.h:
