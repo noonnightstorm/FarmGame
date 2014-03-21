a615bacedebf6bdef31f9c012ede1b5441460fe3
c860 <windwebmaster@163.com>
2014-03-21 17:06:39 +0800

    update

--- Classes/Building/Canteen.cpp
+++ Classes/Building/Canteen.cpp
@@ -6,6 +6,11 @@
  */
 
 #include "Canteen.h"
+#include "cocos-ext.h"
+
+using namespace cocos2d;
+using namespace cocos2d::extension;
+using namespace cocos2d::gui;
 
 Canteen::Canteen() {
 	// TODO Auto-generated constructor stub
@@ -27,5 +32,4 @@ bool Canteen::init() {
 
 void Canteen::onClick()
 {
-	CCLog("canteen!");
 }
